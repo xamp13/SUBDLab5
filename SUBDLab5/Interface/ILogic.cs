1a05b228821ad84af05c4780be3c11dfe3e72abd
xamp13 <den.ohotnikov@gmail.com>
2020-05-25 13:41:52 +0400

    Add Interface

---
+++ SUBDLab5/Interface/ILogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SUBDLab5.Interface
+{
+    public interface ILogic<T>
+    {
+        List<T> Read();
+        void Update(T model);
+        void Create(T model);
+        void Delete(T model);
+        T Get(int Id);
+    }
+}
